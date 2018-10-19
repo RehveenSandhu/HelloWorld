965ee3792f88cd414a67a415af7b71cb93d811c1
Rehveen Sandhu <rehveen.sandhu@jbhifi.com.au>
2018-10-19 14:23:58 +1100

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The life is here and now");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
